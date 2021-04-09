d13478187377895758955e3be5257294d34c35cd
Taejin Jung <taejin5314@gmail.com>
2021-04-09 23:53:15 +0000

    Coding challenge Looping - Range answer

---
+++ range.js
@@ -0,0 +1,17 @@
+function range(start, end, step) {
+  const arr = [];
+  if (start === undefined || end === undefined || step === undefined || start > end || step === 0 || step < 0) {
+    return arr;
+  } else {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+    return arr;
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(0, 0, 0));
+console.log(range(5, 6, 2));
