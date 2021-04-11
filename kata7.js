f6bc02a9b3d5baab45bff71946f631c3e4b420e5
Taejin Jung <taejin5314@gmail.com>
2021-04-11 20:09:13 +0000

    Kata 7 - In the Air Tonight

---
+++ kata7.js
@@ -0,0 +1,19 @@
+const checkAir = function (samples, threshold) {
+  // Code here!
+  let pollution = 0;
+  for (let i = 0; i < samples.length; i++) {
+    if (samples[i] === 'dirty') {
+      pollution++;
+    }
+  }
+  if (pollution / samples.length > threshold) {
+    return 'Polluted';
+  }
+  return 'Clean';
+};
+
+console.log(checkAir(['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'], 0.3));
+
+console.log(checkAir(['dirty', 'dirty', 'dirty', 'dirty', 'clean'], 0.25));
+
+console.log(checkAir(['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'], 0.9));
