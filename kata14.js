e1cecab0f2ae709df6abfcee8db359e1ca087c97
Taejin Jung <taejin5314@gmail.com>
2021-04-12 22:49:03 +0000

    Kata 14 - Change Calculator

---
+++ kata14.js
@@ -0,0 +1,7 @@
+const calculateChange = function (total, cash) {
+  // Your code here
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
