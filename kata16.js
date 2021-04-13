688706b99f2927e53ede11fdbec21ee25a2fb719
Taejin Jung <taejin5314@gmail.com>
2021-04-13 21:42:20 +0000

    Kata 16 - Case Maker II

---
+++ kata16.js
@@ -0,0 +1,13 @@
+const makeCase = function (input, selectedCase) {
+  // Put your solution here
+  for ()
+};
+
+console.log(makeCase('this is a string', 'camel'));
+console.log(makeCase('this is a string', 'pascal'));
+console.log(makeCase('this is a string', 'snake'));
+console.log(makeCase('this is a string', 'kebab'));
+console.log(makeCase('this is a string', 'title'));
+console.log(makeCase('this is a string', 'vowel'));
+console.log(makeCase('this is a string', 'consonant'));
+console.log(makeCase('this is a string', ['upper', 'snake']));
