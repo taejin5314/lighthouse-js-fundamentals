3b53b1acfd3f277861051cb5045cfc7a2cf76f7a
Taejin Jung <taejin5314@gmail.com>
2021-04-14 19:13:10 +0000

    Kata 16 - Case Maker II

--- kata16.js
+++ kata16.js
@@ -11,6 +11,7 @@ const makeCase = function (input, selectedCase) {
   for (let i = 0; i < caseArray.length; i++) {
     let temp = '';
     if (caseArray[i] === 'camel') {
+      // camelCase
       for (let j = 0; j < newString.length; j++) {
         if (newString[j - 1] === ' ') {
           temp += newString[j].toUpperCase();
@@ -20,6 +21,7 @@ const makeCase = function (input, selectedCase) {
       }
       newString = temp;
     } else if (caseArray[i] === 'pascal') {
+      // PascalCase
       for (let j = 0; j < newString.length; j++) {
         if (newString[j - 1] === ' ') {
           temp += newString[j].toUpperCase();
@@ -31,13 +33,62 @@ const makeCase = function (input, selectedCase) {
       }
       newString = temp;
     } else if (caseArray[i] === 'snake') {
+      // snake_case
+      for (let j = 0; j < newString.length; j++) {
+        if (newString[j] === ' ') {
+          temp += '_';
+        } else if (newString[j] !== ' ') {
+          temp += newString[j];
+        }
+      }
+      newString = temp;
     } else if (caseArray[i] === 'kebab') {
+      // kebab-case
+      for (let j = 0; j < newString.length; j++) {
+        if (newString[j] === ' ') {
+          temp += '-';
+        } else if (newString[j] !== ' ') {
+          temp += newString[j];
+        }
+      }
+      newString = temp;
     } else if (caseArray[i] === 'title') {
+      // Title Case
+      for (let j = 0; j < newString.length; j++) {
+        if (newString[j - 1] === ' ') {
+          temp += newString[j].toUpperCase();
+        } else if (j === 0 && newString[j] !== ' ') {
+          temp += newString[j].toUpperCase();
+        } else {
+          temp += newString[j];
+        }
+      }
+      newString = temp;
     } else if (caseArray[i] === 'vowel') {
+      // vOwEl cAsE
+      for (let j = 0; j < newString.length; j++) {
+        if (newString[j] === 'a' || newString[j] === 'e' || newString[j] === 'i' || newString[j] === 'o' || newString[j] === 'u') {
+          temp += newString[j].toUpperCase();
+        } else {
+          temp += newString[j];
+        }
+      }
+      newString = temp;
     } else if (caseArray[i] === 'consonant') {
+      // CoNSoNaNT CaSe
+      for (let j = 0; j < newString.length; j++) {
+        if (newString[j] === 'a' || newString[j] === 'e' || newString[j] === 'i' || newString[j] === 'o' || newString[j] === 'u') {
+          temp += newString[j].toLowerCase();
+        } else {
+          temp += newString[j].toUpperCase();
+        }
+      }
+      newString = temp;
     } else if (caseArray[i] === 'upper') {
+      // UPPERCASE
       newString = newString.toUpperCase();
     } else if (caseArray[i] === 'lower') {
+      // lowercase
       newString = newString.toLowerCase();
     }
   }
