96d7678d6c9c3c18d104d55453427846b79e0f2a
Taejin Jung <taejin5314@gmail.com>
2021-04-14 23:48:23 +0000

    Kata 17 - JS Object From URL Encoded String

--- kata17.js
+++ kata17.js
@@ -1,6 +1,7 @@
 const urlDecode = function (text) {
   let newText = '';
   for (let i = 0; i < text.length; i++) {
+    // convert %20 to ' ' and add it to a new string.
     if (text[i] === '%' && text[i + 1] === '2' && text[i + 2] === '0') {
       newText += ' ';
       i += 2;
@@ -8,15 +9,22 @@ const urlDecode = function (text) {
       newText += text[i];
     }
   }
+  // split the string at the '&' sign.
   let textArray = newText.split('&');
   let textObject = {};
   for (let i = 0; i < textArray.length; i++) {
     for (let j = 0; j < textArray[i].length; j++) {
       if (textArray[i][j] === '=') {
+        // before '=', it will be a key name.
+        let key = textArray[i].slice(0, j);
+        // after '=', it will be a value name.
+        let value = textArray[i].slice(j + 1, textArray[i].length);
+        textObject[key] = value;
+        break;
       }
     }
   }
-  return textArray;
+  return textObject;
 };
 
 console.log(urlDecode('duck=rubber'));
