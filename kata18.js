9e1adea6770c65cfc9cfd012fbddce531a29b647
Taejin Jung <taejin5314@gmail.com>
2021-04-14 23:49:30 +0000

    Kata 18 - Square Code

---
+++ kata18.js
@@ -0,0 +1,8 @@
+const squareCode = function (message) {
+  // Put your solution here
+};
+
+console.log(squareCode('chill out'));
+console.log(squareCode('feed the dog'));
+console.log(squareCode('have a nice day'));
+console.log(squareCode('if man was meant to stay on the ground god would have given us roots'));
