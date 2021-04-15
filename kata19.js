8d61c347162b729e8cfb4e30ba499987307d7bc0
Taejin Jung <taejin5314@gmail.com>
2021-04-15 18:52:42 +0000

    Kata 19 - Queen Threat Detector

---
+++ kata19.js
@@ -0,0 +1,7 @@
+const generateBoard = function (whiteQueen, blackQueen) {};
+
+let whiteQueen = [0, 5];
+let blackQueen = [5, 0];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
