f1c824b9139d1d870d744b4c508da5568cd94c46
Taejin Jung <taejin5314@gmail.com>
2021-04-15 22:26:00 +0000

    Kata 20 - Taxicab geometry

--- kata20.js
+++ kata20.js
@@ -1,5 +1,23 @@
 const blocksAway = function (directions) {
-  // Put your solution here
+  let angle = 0;
+  let currentLocation = { east: 0, north: 0 };
+  for (let i = 0; i < directions.length; i++) {
+    if (i % 2 === 0) {
+      if (directions[i] === 'right') angle += 90;
+      else if (directions[i] === 'left') angle -= 90;
+    } else if (i % 2 === 1) {
+      if (angle < 0) {
+        angle += 360;
+      } else if (angle >= 360) {
+        angle -= 360;
+      }
+      if (angle === 0) currentLocation.north += directions[i];
+      else if (angle === 90) currentLocation.east += directions[i];
+      else if (angle === 180) currentLocation.north -= directions[i];
+      else if (angle === 270) currentLocation.east -= directions[i];
+    }
+  }
+  return currentLocation;
 };
 
 console.log(blocksAway(['right', 2, 'left', 3, 'left', 1]));
