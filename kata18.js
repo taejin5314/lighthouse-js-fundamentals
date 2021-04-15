dd49c8428bf528e0f11f7a4de7a660be23dafc9b
Taejin Jung <taejin5314@gmail.com>
2021-04-15 01:19:56 +0000

    Kata 18 - Square Code

--- kata18.js
+++ kata18.js
@@ -1,5 +1,27 @@
 const squareCode = function (message) {
   // Put your solution here
+  let newMessage = '';
+  for (let i = 0; i < message.length; i++) {
+    if (message[i] !== ' ') {
+      newMessage += message[i];
+    }
+  }
+
+  let squaredMessage = [];
+  const col = Math.ceil(Math.sqrt(newMessage.length));
+  const row = Math.round(newMessage.length / col);
+  for (i = 0; i < row; i++) {
+    squaredMessage.push(newMessage.slice(i * col, (i + 1) * col));
+  }
+
+  let reversedMessage = '';
+  for (i = 0; i < col; i++) {
+    for (let j = 0; j < row; j++) {
+      if (squaredMessage[j][i]) reversedMessage += squaredMessage[j][i];
+    }
+    reversedMessage += ' ';
+  }
+  return reversedMessage;
 };
 
 console.log(squareCode('chill out'));
